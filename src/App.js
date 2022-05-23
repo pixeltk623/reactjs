edffc8ac34dbfd331b9747c03383b8b3a2d5a1a2
Sharvan Kumar <info@pixeltk.com>
2022-05-23 09:31:24 +0530

    Done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import First from './First';
+import NavBar from './NavBar';
+import Employee from './Employee';
+import FirstWithProps from './FirstWithProps';
+import School from './School';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <NavBar />
+      <First />
+      <Employee /> */}
+
+      {/* <FirstWithProps firstName = "Sharvan" lastName = "Kumar" /> */}
+
+      <School />
     </div>
   );
 }
