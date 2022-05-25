4dc43b22f261ad899e05dbc65b13a083e77c8264
pixeltk623 <86755498+pixeltk623@users.noreply.github.com>
2022-05-25 09:35:57 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,22 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import First from './First';
-import NavBar from './NavBar';
-import Employee from './Employee';
-import FirstWithProps from './FirstWithProps';
-import School from './School';
-
+import Employee from './Classbased/Employee';
 
 function App() {
   return (
-    <div className="App">
-      {/* <NavBar />
-      <First />
-      <Employee /> */}
-
-      {/* <FirstWithProps firstName = "Sharvan" lastName = "Kumar" /> */}
-
-      <School />
+    <div className="">
+      <Employee dpt = ".net" />
     </div>
   );
 }
